0ab28d4bdf0d4e05f65aaa057f258edf88d1fe12
dr4g0nsr <dragonmen@gmail.com>
2019-09-26 00:26:21 +0200

    New settings

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker build . -t nginx-php-redis:latest -t nginx-php-redis:1.0 -t mcloud-nginx-php-redis -t dr4g0nsr/nginx-php-redis
+docker build . -t nginx-php-redis:latest -t nginx-php-redis:1.0 -t dr4g0nsr/nginx-php-redis
--- push.sh
+++ push.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+docker build . -t dr4g0nsr/nginx-php-redis
 docker push dr4g0nsr/nginx-php-redis
